19f2d0e7b31a69e8e0d78d57264a6a0f9019dcc4
Javiowo03 <jmp3498@gmail.com>
2026-01-30 20:45:03 +0100

    botones(creo)

--- DIALOGO/ctrlbtn.gd
+++ DIALOGO/ctrlbtn.gd
@@ -1,1 +1,14 @@
 extends Node
+var btn1 = Button.new()
+var btn2 = Button.new()
+var btn3 = Button.new() # or create a button scene instead
+btn1.pressed.connect()
+
+func crtbtn()
+	
+	add_child(btn1)
+	add_child(btn2)
+	add_child(btn3)
+
+func _on_timer_timeout() -> void:
+	
