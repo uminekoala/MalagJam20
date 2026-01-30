c25336dba447f242a390d4f32d3b95844514974a
Javiowo03 <jmp3498@gmail.com>
2026-01-30 23:11:58 +0100

    protobotones

--- DIALOGO/ctrlbtn.gd
+++ DIALOGO/ctrlbtn.gd
@@ -1,14 +1,25 @@
 extends Node
-var btn1 = Button.new()
-var btn2 = Button.new()
-var btn3 = Button.new() # or create a button scene instead
-btn1.pressed.connect()
+#var btn1 = Button.new()
+#var btn2 = Button.new()
+#var btn3 = Button.new() # or create a button scene instead
 
-func crtbtn()
-	
-	add_child(btn1)
-	add_child(btn2)
-	add_child(btn3)
-
-func _on_timer_timeout() -> void:
-	
+@onready var btn1=$"Button1"
+@onready var btn2=$"Button2"
+@onready var btn3=$"Button3"
+func _ready():
+	pass
+func _on_button_1_pressed() -> void:
+	btn2.disabled=true
+	btn2.visible=false
+	btn3.disabled=true
+	btn3.visible=false
+func _on_button_2_pressed() -> void:
+	btn1.disabled=true
+	btn1.visible=false
+	btn3.disabled=true
+	btn3.visible=false
+func _on_button_3_pressed() -> void:
+	btn1.disabled=true
+	btn1.visible=false
+	btn2.disabled=true
+	btn2.visible=false
