dd6cd254619abd5f1c260d730bbf0fcd1055811b
Servelgar <svelazquezg96@gmail.com>
2026-01-31 11:34:33 +0100

    primer bocetillo del main + fiesta

--- scripts/dialogo_labels.gd
+++ scripts/dialogo_labels.gd
@@ -4,11 +4,17 @@ extends ScrollContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.connect("response", on_response)
+	Global.connect("first_response",on_first_response)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
+func on_first_response():
+	$VBoxContainer/MoonNameLabel.visible_characters = -1
+	$VBoxContainer/MoonDialogueLabel.text = "HAZ TU MAGIA ANDREA + TRANSICION + BAILE + SHADERS!!!!!"
+	$VBoxContainer/MoonDialogueLabel/AnimationPlayer.play("texto")
 
 func on_response(line):
 	$VBoxContainer/MoonNameLabel.visible_characters = -1
