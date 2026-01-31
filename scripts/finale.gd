3f766afab7681952550320633e22d7dd17ec1a15
Javiowo03 <jmp3498@gmail.com>
2026-02-01 00:39:08 +0100

    finaleavanza

--- scripts/finale.gd
+++ scripts/finale.gd
@@ -1,16 +1,18 @@
 extends ScrollContainer
 #este es el de la puntuacion
 var finished_sentence = false
-
-
 # Called when the node enters the scene tree for the first time.
+var point=0
 func _ready() -> void:
-	Global.connect("puntuacion", punt)
+	Global.connect("finish", puntobt)
 	punt()
 	puntuacion()
 func punt():
 	print("puntuacion")
 	$VBoxContainer/textpunt.text = "Tu puntuación es:"
 	$VBoxContainer/textpunt/AnimationPlayerpunt.play("texto")
-func puntuacion():
+func puntobt():
 	pass
+func puntuacion():
+	$VBoxContainer/punt.text = "holo"
+	$VBoxContainer/punt/AnimationPlayerpuntu.play("texto")
