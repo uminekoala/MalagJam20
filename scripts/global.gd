f000f6290d581fa68088500f4b86e22b3136e0a8
Servelgar <svelazquezg96@gmail.com>
2026-01-31 13:23:43 +0100

    muerte en las vegas

--- scripts/global.gd
+++ scripts/global.gd
@@ -5,8 +5,11 @@ signal response(line)
 signal send_text_buttons(line0,line1,line2)
 signal end_dialogue()
 signal first_response()
-signal change_scene_to_dialogue()
+# primero transicion (mover la mascara), luego mecánicas
+signal transition_to_dialogue()
+signal transition_to_dance()
 signal change_scene_to_dance()
+signal change_scene_to_dialogue()
 
 
 # Called when the node enters the scene tree for the first time.
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,12 @@
 extends Node
 
 var tween_movement_luna
+var tween_visible
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.connect("change_scene_to_dialogue", on_to_dialogue)
-	Global.connect("change_scene_to_dance", on_to_dance)
+	Global.connect("transition_to_dialogue", on_transition_to_dialogue)
+	Global.connect("transition_to_dance", on_transition_to_dance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,14 +14,18 @@ func _process(delta: float) -> void:
 	pass
 
 
-func on_to_dialogue():
+func on_transition_to_dialogue():
 	#apagar todo lo que sea de danzadura
 	# Animar sprite para que se mueva a la izquierda
 	move_luna(false)
 	# llamar al dialogo
-	
+	if (tween_visible):
+		tween_visible.kill()
+	tween_visible = get_tree().create_tween()
+	# escena de los botones
+	tween_visible.connect("finished",send_change_scene_to_dance_signal)
 
-func on_to_dance():
+func on_transition_to_dance():
 	# apagar todo lo que sea de dialogos
 	if ($FirstButton):
 		$FirstButton.queue_free()
@@ -29,7 +34,18 @@ func on_to_dance():
 	# Animar sprite para que se mueva a la derecha
 	move_luna(true)
 	# llamar a la danzadura
+	if (tween_visible):
+		tween_visible.kill()
+	tween_visible = get_tree().create_tween()
+	tween_visible.tween_property($MinijuegoBaile, "visible", true, 1.0)
+	tween_visible.connect("finished",send_change_scene_to_dialogue_signal)
 
+func send_change_scene_to_dialogue_signal():
+	Global.change_scene_to_dialogue.emit()
+
+
+func send_change_scene_to_dance_signal():
+	Global.change_scene_to_dance.emit()
 
 func move_luna(is_right):
 	if (tween_movement_luna):
