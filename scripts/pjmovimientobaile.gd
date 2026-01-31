36e49b287621c307ec01d8c65c6a8d0fa8d9ea44
Azzazel <138659938+AngelVF26@users.noreply.github.com>
2026-01-31 23:20:40 +0100

    sounds cock

--- scripts/pjmovimientobaile.gd
+++ scripts/pjmovimientobaile.gd
@@ -4,43 +4,47 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var col_arriba: CollisionShape2D = $"../ColArriba"
-@onready var pisada: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var p_jbaile: CharacterBody2D = %PJbaile
+@onready var pisada: AudioStreamPlayer2D = $AudioStreamPlayer2D2
+@onready var pisoton: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var moverse:bool = false
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("tilemapArriba"):
-		if global_position.y > 400 && moverse:
-			position.y -= 256
-			moverse = false
-			pisada.play()
-	if Input.is_action_just_pressed("tilemapAbajo"):
-		if global_position.y < 680 && moverse:
-			position.y += 256
-			moverse = false
-			pisada.play()
-	if Input.is_action_just_pressed("tilemapDerecha"):
-		if global_position.x < 680 && moverse:
-			position.x += 256
-			moverse = false
-			pisada.play()
-	if Input.is_action_just_pressed("tilemapIzquierda"):
-		if global_position.x > 380 && moverse:
-			position.x -= 256
-			moverse = false
-			pisada.play()
-	if Input.is_action_just_pressed("espacio"):
-		$AnimatedSprite2D.visible = true
-		$AnimatedSprite2D2.visible = true
-		$AnimatedSprite2D3.visible = true
-		$AnimatedSprite2D4.visible = true
-		$AnimatedSprite2D.play("default")
-		$AnimatedSprite2D2.play("default")
-		$AnimatedSprite2D3.play("default")
-		$AnimatedSprite2D4.play("default")
-		$AnimationPlayer.play("MOVE")
-		$Timer.start()
-		
+	if p_jbaile.visible == true:
+		if Input.is_action_just_pressed("tilemapArriba"):
+			if global_position.y > 400 && moverse:
+				position.y -= 256
+				moverse = false
+				pisada.play()
+		if Input.is_action_just_pressed("tilemapAbajo"):
+			if global_position.y < 680 && moverse:
+				position.y += 256
+				moverse = false
+				pisada.play()
+		if Input.is_action_just_pressed("tilemapDerecha"):
+			if global_position.x < 680 && moverse:
+				position.x += 256
+				moverse = false
+				pisada.play()
+		if Input.is_action_just_pressed("tilemapIzquierda"):
+			if global_position.x > 380 && moverse:
+				position.x -= 256
+				moverse = false
+				pisada.play()
+		if Input.is_action_just_pressed("espacio"):
+			pisoton.play()
+			$AnimatedSprite2D.visible = true
+			$AnimatedSprite2D2.visible = true
+			$AnimatedSprite2D3.visible = true
+			$AnimatedSprite2D4.visible = true
+			$AnimatedSprite2D.play("default")
+			$AnimatedSprite2D2.play("default")
+			$AnimatedSprite2D3.play("default")
+			$AnimatedSprite2D4.play("default")
+			$AnimationPlayer.play("MOVE")
+			$Timer.start()
+
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
