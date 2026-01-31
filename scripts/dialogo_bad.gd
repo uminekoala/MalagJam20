b6dfd3068dab78045e6e35ba48723a5cd4fd6b83
Dew <andrea.rs.98@gmail.com>
2026-01-31 12:55:44 +0100

    Audioo

--- scripts/dialogo_bad.gd
+++ scripts/dialogo_bad.gd
@@ -4,7 +4,7 @@ extends GPUParticles2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	emitting = true
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
