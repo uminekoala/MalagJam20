268284e2754ed9af3e31f71eff93a4f6dda3e5c9
Javikh33 <Javikhtrainer@gmail.com>
2026-02-01 10:33:41 +0100

    TUTORIAL

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -3,7 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$MinijuegoBaile.funcionar = true
+	$MinijuegoBaile/CasillaBaile/AnimatedSprite2D.visible = true
+	$MinijuegoBaile/CasillaBaile/AnimationPlayer.active = true
+	$MinijuegoBaile/CasillaBaile/AnimationPlayer.play("fade_in")
+	DialogoController.puedes_moverte_don_jugador = true
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,12 +18,20 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	
+	$MinijuegoBaile.funcionar = false
+	$MinijuegoBaile/CasillaBaile/AnimatedSprite2D.visible = false
+	$MinijuegoBaile/CasillaBaile/AnimationPlayer.active = false
+	DialogoController.puedes_moverte_don_jugador = false
+	$MinijuegoBaile/CasillaBaile/AnimationPlayer.stop()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_2_pressed() -> void:
-	
+	$MinijuegoBaile.funcionar = false
+	$MinijuegoBaile/CasillaBaile/AnimatedSprite2D.visible = false
+	$MinijuegoBaile/CasillaBaile/AnimationPlayer.active = false
+	DialogoController.puedes_moverte_don_jugador = false
+	$MinijuegoBaile/CasillaBaile/AnimationPlayer.stop()
 	get_tree().change_scene_to_file("res://scenes/tutorial_dialogo.tscn")
 	pass # Replace with function body.
