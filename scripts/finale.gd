4f0377de5fab70c730f102a3b897b59c8099d6e8
Javiowo03 <jmp3498@gmail.com>
2026-02-01 01:27:33 +0100

    finalpuntos

--- scripts/finale.gd
+++ scripts/finale.gd
@@ -4,15 +4,15 @@ var finished_sentence = false
 # Called when the node enters the scene tree for the first time.
 var point=0
 func _ready() -> void:
-	Global.connect("finish", puntobt)
 	punt()
 	puntuacion()
 func punt():
 	print("puntuacion")
 	$VBoxContainer/textpunt.text = "Tu puntuación es:"
 	$VBoxContainer/textpunt/AnimationPlayerpunt.play("texto")
-func puntobt():
-	pass
+func puntobt(point):
+	Global.puntos
+	
 func puntuacion():
-	$VBoxContainer/punt.text = "holo"
+	$VBoxContainer/punt.text = str(Global.puntos)
 	$VBoxContainer/punt/AnimationPlayerpuntu.play("texto")
