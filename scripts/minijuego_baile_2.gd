ad88a176a048592bd7c9a799df8ee32f616de302
Javikh33 <72210148+Javikh33@users.noreply.github.com>
2026-02-07 03:28:57 +0100

    bailado

--- scripts/minijuego_baile_2.gd
+++ scripts/minijuego_baile_2.gd
@@ -8,18 +8,30 @@ var fase: int = 0
 @onready var feedback: Label = $PJbaile/Label
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 var enArea: bool = false
+#var vectorPrueba: Vector2 = Vector2(2,1)
+var posibilidades: Array
+var arraySobrante: Array
+var j: int
+var angle = deg_to_rad(0)
+#var radius = vectorPrueba.distance_to(Vector2(vectorPrueba.x,vectorPrueba.y+2))
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.connect("change_scene_to_dance", _on_global_change_scene_to_dance)
 
-
+var seno
+var coseno
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	%PJbaile
-	#if !funcionar:
-	#	Global.connect("change_scene_to_dance", _on_global_change_scene_to_dance())
-	
+	#print("empezamos : " + str(vectorPrueba))
+	#print("radio: " + str(radius))
+	#for i in range(0, 8):
+		##print("angle : " + str(45*i))
+		#coseno = round((cos(deg_to_rad(45*i)) * radius))
+		#seno = round((sin(deg_to_rad(45*i)) * radius))
+		#calculos(vectorPrueba)
+			
+	#print("terminamos : ")
 	$Label2.text = str(Global.puntos)
 	$Label.text = str($CasillaBaile/AnimationPlayer/Timer.time_left).pad_decimals(2)
 	if Input.is_action_just_pressed("espacio"):
@@ -61,11 +73,21 @@ func _process(delta: float) -> void:
 		$CasillaBaile/Area2D/CollisionShape2D.disabled = true
 		
 
-
+func calculos(vectorCalc: Vector2) -> void:
+	var vectorCalcInterno = vectorCalc - Vector2(vectorCalc +Vector2(coseno, seno)).direction_to(vectorCalc)
+	if vectorCalc.distance_to(vectorCalc +Vector2(coseno, seno)) <= 2:
+		if vectorCalc.distance_to(vectorCalc +Vector2(coseno, seno)) == 2:
+			if vectorCalcInterno.x <=2 && vectorCalcInterno.x  >=0 && vectorCalcInterno.y <=2 && vectorCalcInterno.y >=0:
+				posibilidades.append(vectorCalc - Vector2(vectorCalc +Vector2(coseno, seno)).direction_to(vectorCalc))
+		if vectorCalc.x + coseno <=2 && vectorCalc.x + coseno >=0 && vectorCalc.y + seno <=2 && vectorCalc.y + seno >=0:
+			print("Vector en rango : " + str(vectorCalc + Vector2(coseno, seno)))
+			posibilidades.append(Vector2(vectorCalc + Vector2(coseno, seno)))
+		
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	print("OH NO??")
 	if posicion < casillasPrimeraFase:
-		print(%PJbaile.global_position)
+		print("la global" + str(%PJbaile.global_position))
+		print("la local " + str(%PJbaile.position))
 		dondeAndaras = tuMovimiento(%PJbaile.global_position)
 		print("a veer : " + str(dondeAndaras))
 		%CasillaBaile.global_position = Vector2((dondeAndaras.x * 224) + 112, (dondeAndaras.y * 224) + 96)
@@ -90,33 +112,16 @@ func _on_animation_player_animation_started(anim_name: StringName) -> void:
 func tuMovimiento(movimiento: Vector2):
 	movimiento = Vector2((movimiento.x + 2) / 224, (movimiento.y + 16 ) / 224) - Vector2(1,1)
 	print("este es el mov: " + str(movimiento))
-	if movimiento == Vector2(2,2):
-		movimiento = [Vector2(1,2),Vector2(2,1),Vector2(2,0),Vector2(1,1),Vector2(0,2)].pick_random()
-		return movimiento
-	if movimiento == Vector2(2,1):
-		movimiento = [Vector2(2,0),Vector2(2,2),Vector2(1,0),Vector2(0,1),Vector2(1,2)].pick_random()
-		return movimiento
-	if movimiento == Vector2(2,0):
-		movimiento = [Vector2(1,0),Vector2(2,1),Vector2(0,0),Vector2(1,1),Vector2(2,2)].pick_random()
-		return movimiento
-	if movimiento == Vector2(1,2):
-		movimiento = [Vector2(2,2),Vector2(0,2),Vector2(1,0),Vector2(2,1),Vector2(0,1)].pick_random()
-		return movimiento
-	if movimiento == Vector2(1,1):
-		movimiento = [Vector2(2,1),Vector2(1,0),Vector2(0,0),Vector2(2,0),Vector2(0,2),Vector2(2,2)].pick_random()
-		return movimiento
-	if movimiento == Vector2(1,0):
-		movimiento = [Vector2(2,0),Vector2(0,0),Vector2(0,1),Vector2(1,2),Vector2(2,1)].pick_random()
-		return movimiento
-	if movimiento == Vector2(0,2):
-		movimiento = [Vector2(1,2),Vector2(0,1),Vector2(0,0),Vector2(1,1),Vector2(2,2)].pick_random()
-		return movimiento
-	if movimiento == Vector2(0,1):
-		movimiento = [Vector2(0,2),Vector2(0,0),Vector2(2,1),Vector2(1,2),Vector2(1,0)].pick_random()
-		return movimiento
-	if movimiento == Vector2(0,0):
-		movimiento = [Vector2(0,1),Vector2(1,0),Vector2(2,0),Vector2(1,1),Vector2(0,2)].pick_random()
-		return movimiento
+	var radius = movimiento.distance_to(Vector2(movimiento.x,movimiento.y+2))
+	for i in range(0, 8):
+		coseno = round((cos(deg_to_rad(45*i)) * radius))
+		seno = round((sin(deg_to_rad(45*i)) * radius))
+		calculos(movimiento)
+	print(str(posibilidades))
+	movimiento = posibilidades.pick_random()
+	print("ESTE ES EL NUEVO MOV: " + str(movimiento))
+	posibilidades.clear()
+	return movimiento
 
 func _on_global_transition_to_dialogue() -> void:
 	audio_stream_player_2d.volume_db = -15
